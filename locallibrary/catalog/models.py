47d778deb79f73980fbf831d1de28ba8b7e7abd7
iftekhar joy <chowdhuryiftekharulislam@outlook.com>
2019-02-05 00:12:28 +0600

    Book and Genre class implemented

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
+
+class Book(models.Model):
+    # Model represents a book
+    title = models.CharField(max_length=200)
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    author=models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book.')
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
 
 # Create your models here.
+class Genre(models.Model):
+    # this model represents book genre
+    name = models.CharField(max_length=200, help_text='Enter a book genre')
+
+    #methods
+    def __str__(self):
+        # this method returns Genre class attribute
+        return self.name
